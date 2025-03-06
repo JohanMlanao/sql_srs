f901d69d7b7e5d6eb98db2aa0339e98f3aacef97
jmlanao <johan3333@hotmail.fr>
2025-03-06 15:14:18 +0100

    Correction of the CSV file

--- app.py
+++ app.py
@@ -5,18 +5,18 @@ import io
 
 
 csv = '''
-beverage, price
-orange juice, 2.5
-Expresso, 2
-Tea, 3
+beverage,price
+orange juice,2.5
+Expresso,2
+Tea,3
 '''
 beverages = pd.read_csv(io.StringIO(csv))
 
 csv2 = '''
-food_item, food_price
-cookie juice, 2.5
-chocolatine, 2
-muffin, 3
+food_item,food_price
+cookie juice,2.5
+chocolatine,2
+muffin,3
 '''
 food_items = pd.read_csv(io.StringIO(csv2))
 
