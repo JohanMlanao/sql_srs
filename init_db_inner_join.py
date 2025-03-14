e0953d49c22a8d2747d52a5fb7325af0e03ed9d4
jmlanao <johan3333@hotmail.fr>
2025-03-14 17:33:39 +0100

    Add function to retrieve data

--- init_db_inner_join.py
+++ init_db_inner_join.py
@@ -2,35 +2,35 @@ import io
 
 import pandas as pd
 
-data = {
-        "theme": ["inner_joins", "inner_joins", "inner_joins", "inner_joins"],
-        "exercise_name": [
-            "salaries_and_seniority",
-            "sizes_and_trademarks",
-            "hours_and_minutes",
-            "real_life_case",
-        ],
-        "tables": [
-            ["salaries", "seniority"],
-            ["sizes", "trademarks"],
-            ["hours", "minutes"],
-            ["stores", "months", "days", "markets", "quarters"],
-        ],
-        "last_reviewed": ["1980-01-01", "1970-01-01", "1970-01-01", "1970-01-01"],
-    }
-pd.DataFrame(data)
 
-salaries = '''
+def get_memory_state_inner_join():
+    """
+    Create and return a pandas DataFrame containing all the basic information for all inner join exercises
+    """
+    data = {
+        "theme": ["inner_joins"],
+        "exercise_name": ["salaries_and_seniority"],
+        "tables": [["salaries", "seniority"]],
+        "last_reviewed": ["1980-01-01"],
+    }
+    return pd.DataFrame(data)
+
+
+def get_salaries_and_seniority():
+    """
+    Create and return salaries and seniority tables as pandas DataFrames
+    """
+    salaries = """
 salary,employee_id
 2000,1
 2500,2
 2200,3
-'''
-salaries = pd.read_csv(io.StringIO(salaries))
-
-seniority = '''
+"""
+    salaries = pd.read_csv(io.StringIO(salaries))
+    seniority = """
 employee_id,seniority
 1,2ans
 2,4ans
-'''
-seniority = pd.read_csv(io.StringIO(seniority))
+"""
+    seniority = pd.read_csv(io.StringIO(seniority))
+    return salaries, seniority
