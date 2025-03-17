ee73a24cea15277f17f8394edefd302cbb1ae18c
jmlanao <johan3333@hotmail.fr>
2025-03-17 16:57:46 +0100

    Select specific columns

--- answers/Salaries and seniority.sql
+++ answers/Salaries and seniority.sql
@@ -1,11 +1,11 @@
-SELECT *
+SELECT salary, employee_id, seniority
 FROM salaries
 INNER JOIN seniority
 ON salaries.employee_id = seniority.employee_id
 
 -- OR
 
---SELECT *
+--SELECT salary, employee, seniority
 -- FROM salaries
 -- CROSS JOIN seniority
 -- WHERE salaries.employee_id = seniority.employee_id
