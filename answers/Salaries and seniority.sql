4afafede83206cd0bd12e37959bdf7c308d26a28
jmlanao <johan3333@hotmail.fr>
2025-03-17 17:19:47 +0100

    Correct typo

--- answers/Salaries and seniority.sql
+++ answers/Salaries and seniority.sql
@@ -5,7 +5,7 @@ ON sal.employee_id = sen.employee_id
 
 -- OR
 
---SELECT salary, sal.employee, seniority
+--SELECT salary, sal.employee_id, seniority
 -- FROM salaries as sal
 -- CROSS JOIN seniority as sen
 -- WHERE sal.employee_id = sen.employee_id
